38464df3532fb337963e2e371a5444f2db37d4c7
Koodies <kwtan@outlook.sg>
2022-04-14 14:49:36 +0800

    ADD dynamic content

--- src/components/ExpenseItem/index.js
+++ src/components/ExpenseItem/index.js
@@ -1,12 +1,15 @@
 import './index.css'
 
 function ExpenseItem() {
+    const expenseDate = new Date().toLocaleDateString("en-US")
+    const expenseTitle = 'Car Insurance'
+    const expenseAmount = 294.67
     return (
         <div className='expense-item'>
-            <div>April 13th 2022</div>
+            <div>{expenseDate}</div>
             <div className='expense-item__description'>
-                <h2>Car Insurance</h2>
-                <div className='expense-item__price'>$294.67</div>
+                <h2>{expenseTitle}</h2>
+                <div className='expense-item__price'>{expenseAmount}</div>
             </div>
         </div>
     )
