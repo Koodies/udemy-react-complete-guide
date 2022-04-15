c9248eda9476dd8da22eaf9a7c67a6f8c4f6e83e
Koodies <kwtan@outlook.sg>
2022-04-15 15:51:38 +0800

    init "Add new Expense Form"

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import logo from './logo.svg';
 import './App.css';
 import Expenses from './components/Expenses';
+import NewExpense from './components/NewExpense';
 
 function App() {
 
   return (
     <div className="App">
       <header className="App-header">
+        <NewExpense/>
         <Expenses/>
       </header>
     </div>
