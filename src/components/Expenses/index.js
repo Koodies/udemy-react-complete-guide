5bb22567d1596486822f8bc8dfe2f96f88e77cf9
Koodies <kwtan@outlook.sg>
2022-04-15 16:53:26 +0800

    Add a filter dropdown box

--- src/components/Expenses/index.js
+++ src/components/Expenses/index.js
@@ -1,11 +1,18 @@
 import './index.css'
+import { useState } from 'react'
 import ExpenseItem from './ExpenseItem'
 import Card from '../Card'
+import ExpensesFilter from './ExpensesFilter'
 
 function Expenses(props) {
     const expenses = props.expenses
+    const [filteredYear, setFilteredYear] = useState('2020')
+    const filterByYearHandler = (year) => {
+        setFilteredYear(year)
+    }
     return (
         <Card className="expenses">
+            <ExpensesFilter selected={filteredYear} filterByYear={filterByYearHandler}/>
             {expenses.map(expense => {
                 return <ExpenseItem title={expense.title} amount={expense.amount} date={expense.date} />
             })}
