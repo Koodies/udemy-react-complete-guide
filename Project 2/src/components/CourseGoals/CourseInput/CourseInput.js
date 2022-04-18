f9e9efd4d672df57e4462db637586afd950b70d0
Koodies <kwtan@outlook.sg>
2022-04-18 12:12:52 +0800

    CNG to module.css

--- Project 2/src/components/CourseGoals/CourseInput/CourseInput.js
+++ Project 2/src/components/CourseGoals/CourseInput/CourseInput.js
@@ -1,5 +1,6 @@
 import React, { useState } from 'react';
-import styled from 'styled-components';
+//import styled from 'styled-components';
+import styles from './CourseInput.module.css'
 
 import Button from '../../UI/Button/Button';
 import './CourseInput.css';
@@ -22,42 +23,42 @@ const CourseInput = props => {
     props.onAddGoal(enteredValue);
   };
 
-  const FormControl = styled.div`
-    margin: 0.5rem 0;
+  // const FormControl = styled.div`
+  //   margin: 0.5rem 0;
     
-    & label {
-      font-weight: bold;
-      display: block;
-      margin-bottom: 0.5rem;
-    }
+  //   & label {
+  //     font-weight: bold;
+  //     display: block;
+  //     margin-bottom: 0.5rem;
+  //   }
     
-    & input {
-      display: block;
-      width: 100%;
-      border: 1px solid #ccc;
-      font: inherit;
-      line-height: 1.5rem;
-      padding: 0 0.25rem;
-    }
+  //   & input {
+  //     display: block;
+  //     width: 100%;
+  //     border: 1px solid #ccc;
+  //     font: inherit;
+  //     line-height: 1.5rem;
+  //     padding: 0 0.25rem;
+  //   }
     
-    & input:focus {
-      outline: none;
-      background: #fad0ec;
-      border-color: #8b005d;
-    }
+  //   & input:focus {
+  //     outline: none;
+  //     background: #fad0ec;
+  //     border-color: #8b005d;
+  //   }
     
-    &.invalid input {
-      border-color: red;
-      background: #ffd7d7
-    }
-  `;
+  //   &.invalid input {
+  //     border-color: red;
+  //     background: #ffd7d7
+  //   }
+  // `;
 
   return (
     <form onSubmit={formSubmitHandler}>
-      <FormControl className={!isValid && 'isInvalid'}>
+      <div className={`${styles['form-control']} ${!isValid && styles.invalid}`}>
         <label>Course Goal</label>
         <input type="text" onChange={goalInputChangeHandler} />
-      </FormControl>
+      </div>
       <Button type="submit">Add Goal</Button>
     </form>
   );
